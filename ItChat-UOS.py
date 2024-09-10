ca2237d0c0fce6224036892e483137f43c242621
Sakura <1467649772@qq.com>
2024-09-10 19:19:13 +0800

    push

--- ItChat-UOS.py
+++ ItChat-UOS.py
@@ -1,5 +1,28 @@
 import itchat
+import schedule
+import time
 
+# 登录微信
 itchat.auto_login()
 
-itchat.send('Hello, filehelper', toUserName='filehelper')
+
+# 获取群聊名称（根据实际情况修改）
+@itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
+def text_reply(msg):
+    if msg['Text'] == '获取群聊名称':
+        return msg['User']['NickName']
+
+
+# 定时发送消息
+def send_message():
+    # 获取群聊唯一标识（通过上述方法获取）
+    chatroom_id = '你的群聊唯一标识'
+    itchat.send('这是定时发送的消息', toUserName=chatroom_id)
+
+
+# 每小时发送一次
+schedule.every().hour.do(send_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
