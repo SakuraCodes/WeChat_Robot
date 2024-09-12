892254263732773b35375bd3100e4af014f079cd
Sakura <1467649772@qq.com>
2024-09-12 14:43:12 +0800

    push

--- SendMessage.py
+++ SendMessage.py
@@ -2,182 +2,122 @@ from wxauto import WeChat
 import schedule
 import time
 
-# 发送对象列表
-listen_list = [
-    # '测试群'
-    '宁波歪麦霸王餐福利群002',
-    '宁波歪麦霸王餐福利群003',
-    '宁波歪麦霸王餐福利群004',
-    '在宁波0-5元吃霸王餐-A8',
-    '在宁波0-5元吃霸王餐-A9',
-    '宁波福利群-100',
-    '宁波福利群-101'
-]
 
-# At人员列表
-at_list = [
-    '桜'
-]
-
-
-def PushBreakfast():
+def PushMsg(msg_list, filepath):
     # 获取微信窗口对象
     wx = WeChat()
-    # 发送消息列表
-    message_list = [
-        # '这是个测试消息'
-        '早上好！新的一天，从一顿美味的霸王餐开始吧！🌞\n店铺已选好，就等你啦！\n🥛真味豆浆\n🈵20返14\n🥚懂包帝·包子豆浆\n🈵20返10\n🥟咬不得高祖生煎\n🈵30返15\n🥙绍兴小笼\n🈵20返10\n🍚三米粥铺\n🈵20返11\n霸王餐链接：👇\n6url.cn/C8XKHh \n————՞•・•՞————\n[庆祝]上线参与有机会把iPad、拍立得等开学礼包带回家！\n活动：sourl.cn/PGDZa2'
-    ]
-    # 发送文件
-    files = [
-        # r'F:\!Code\Code_Python\WeChat-Robot\pic\breakfast.jpg',  # 图片
-        r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\breakfast.jpg',  # 图片
-        # r'C:\Users\user\Desktop\2.txt',  # 文件
-        # r'C:\Users\user\Desktop\3.mp4'  # 视频
-    ]
+    for la in listen_atall_list:
+        for m in msg_list:
+            wx.AtAll(msg=m, who=la)
+            wx.SendFiles(filepath=filepath, who=la)
     for l in listen_list:
-        for m in message_list:
+        for m in msg_list:
             wx.SendMsg(msg=m, who=l)
-            # wx.AtAll(msg=m, who=l)
-            wx.SendFiles(filepath=files, who=l)
+            wx.SendFiles(filepath=filepath, who=l)
 
 
-def PushLunch():
-    # 获取微信窗口对象
-    wx = WeChat()
-    # 发送消息列表
-    message_list = [
-        # '【🔔歪麦资讯】\n①、\n②、'
-        # '中午好！是时候用一顿丰盛的霸王餐来犒劳自己了！🍽️\n店铺已选好，就等你啦！\n🍛鮨·渔知精致料理\n🈵20返12\n🥘食光解馋坊\n🈵20返15\n🍲会飞不二(创新128广场店)\n🈵25返15\n🥙尝态·健康创意简餐\n🈵25返15\n🍳来一餐木桶饭第一档口\n🈵20返15\n霸王餐链接：👇\n6url.cn/C8XKHh\n————՞•・•՞————\n[庆祝]上线参与有机会把iPad、拍立得等开学礼包带回家！\n活动：sourl.cn/PGDZa2'
-        '【🔔歪麦资讯】\n• 进入歪麦霸王餐app可享受139.9元全网最低价必胜客自助\n今日午餐任务🥘🍲🍱🥙🍔\n你想好吃什么了吗？\n\n西北阁(炒饭·炒面·炒菜)\n🈵20返14\n\n🐥兴宁桥烤鸡(邱隘店)\n🈵20返15\n\n🥘食光解馋坊\n🈵20返15\n\n🥙尝态·健康创意简餐\n🈵25返15\n\n🍳来一餐木桶饭第一档口\n🈵20返15\n带你0-5元点一顿豪华外卖\n-----------------\n⬇快来点链接选店~👇\n6url.cn/C8XKHh\n————՞•・•՞————\n[庆祝]上线参与有机会把iPad、拍立得等开学礼包带回家！\n活动：sourl.cn/PGDZa2'
+if __name__ == '__main__':
+    # 发送对象列表
+    listen_atall_list = [
+        # '测试群'
+        '宁波歪麦霸王餐福利群002',
+        '宁波歪麦霸王餐福利群003',
+        '宁波歪麦霸王餐福利群004',
+        '在宁波0-5元吃霸王餐-A8'
     ]
-    # 发送文件
-    files = [
-        # r'F:\!Code\Code_Python\WeChat-Robot\pic\dinner.jpg',  # 图片
-        r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\dinner.jpg',  # 图片
-        # r'C:\Users\user\Desktop\2.txt',  # 文件
-        # r'C:\Users\user\Desktop\3.mp4'  # 视频
+    listen_list = [
+        # '测试群'
+        '在宁波0-5元吃霸王餐-A9',
+        '宁波福利群-100',
+        '宁波福利群-101'
     ]
-    for l in listen_list:
-        for m in message_list:
-            wx.SendMsg(msg=m, who=l)
-            # wx.AtAll(msg=m, who=l)
-            wx.SendFiles(filepath=files, who=l)
 
 
-def PushTea():
-    # 获取微信窗口对象
-    wx = WeChat()
-    # 发送消息列表
-    message_list = [
-        # '🧁'
-        # '限时限量！下午茶霸王餐，手慢无！🍮\n店铺已选好，就等你啦！\n🧋蜜雪冰城(宁波通用)\n🈵20返10\n🍹榴小可榴莲饮品\n🈵20返12\n🍸️喜茶(宁波海曙龙湖天街店)\n🈵20返10\n🥃F·CAFE\n🈵25返12\n🥤273C(带梦陶然店)\n🈵30返15\n霸王餐链接：👇\n6url.cn/C8XKHh\n————՞•・•՞————\n[庆祝]上线参与有机会把iPad、拍立得等开学礼包带回家！\n活动：sourl.cn/PGDZa2'
-        '犯困可真是个喝奶茶的好理由呀[嘿哈]\n🧋蜜雪冰城(宁波通用)\n🈵20返10\n\n🍸️喜茶(宁波海曙龙湖天街店)\n🈵20返10\n\n🍹榴小可榴莲饮品\n🈵20返12\n\n🥃F·CAFE\n🈵25返12\n\n🍮山奈(咖啡·甜点)\n🈵20返13\n霸王餐链接：👇\n6url.cn/C8XKHh\n————՞•・•՞————\n[庆祝]上线参与有机会把iPad、拍立得等开学礼包带回家！\n活动：sourl.cn/PGDZa2'
-    ]
-    # 发送文件
-    files = [
-        # r'F:\!Code\Code_Python\WeChat-Robot\pic\tea.jpg',  # 图片
-        r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\tea.jpg',  # 图片
-        # r'C:\Users\user\Desktop\2.txt',  # 文件
-        # r'C:\Users\user\Desktop\3.mp4'  # 视频
-    ]
-    for l in listen_list:
-        for m in message_list:
-            wx.SendMsg(msg=m, who=l)
-            # wx.AtAll(msg=m, who=l)
-            wx.SendFiles(filepath=files, who=l)
+    def PushBreakfast():
+        # 消息列表
+        msg_list = [
+            # '这是个测试消息'
+            '🏆 【霸王餐来袭】 🏆\n🥪 一日之计在于晨，一顿霸王餐让你全天精神百倍！\n\n真味豆浆\n🈵20返14\n懂包帝·包子豆浆\n🈵20返10\n咬不得高祖生煎\n🈵30返15\n绍兴小笼\n🈵20返10\n三米粥铺\n🈵20返11\n------------------\n⬇快来点链接选店啵~👇\n6url.cn/C8XKHh\n\n——————՞•・•՞——————\n🔥每日持续参与，赢取iPad、拍立得等开学礼包\n👉sourl.cn/PGDZa2'
+        ]
+        # 文件列表
+        filepath = [
+            # r'F:\!Code\Code_Python\WeChat-Robot\pic\breakfast.jpg'
+            r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\breakfast.jpg'
+        ]
+        PushMsg(msg_list, filepath)
 
 
-def PushSupper():
-    # 获取微信窗口对象
-    wx = WeChat()
-    # 发送消息列表
-    message_list = [
-        # '这是个早餐测试消息'
-        # '晚上好！一天的忙碌过后，是时候用一顿丰盛的晚餐来放松自己了！🌙\n店铺已选好，就等你啦！\n🍛鮨·渔知精致料理\n🈵20返12\n🥘食光解馋坊\n🈵20返15\n🍲会飞不二(创新128广场店)\n🈵25返15\n🥙尝态·健康创意简餐\n🈵25返15\n🍳来一餐木桶饭第一档口\n🈵20返15\n霸王餐链接：👇\nhttps://6url.cn/C8XKHh\n————՞•・•՞————\n邀请好友吃霸王餐 领20R奖励'
-        # '下班干饭，让我用美食驱赶一天的疲惫！\n\n🥘久久丫·鸭脖(古林店)\n🈵15返12\n\n兴宁桥烤鸡(邱隘店)\n🈵20返15\n\n食光解馋坊\n🈵20返15\n\n西北阁(炒饭·炒面·炒菜)\n🈵20返14\n\n高大乐桂林米粉(128广场店)\n🈵30返15\n\n[红包]『歪麦』成本价点外卖\n👉https://6url.cn/fEGtuw\n————՞•・•՞————\n [庆祝]上线参与0元许愿，点餐就把iPad、拍立得等开学礼包带回家！今天参与明天14：30开奖哦~详情入口：https://sourl.cn/PGDZa2'
-        # '📣【吃晚餐啦】超值新品上线\n🥰麻辣烫🥘，猪脚饭🍱，轻食🥙，面食🍜...应有尽有\n\n🥓久久丫·鸭脖(古林店)\n🈵15返12\n—— —— —— —— ——\n🍲西北阁(炒饭·炒面·炒菜)\n🈵20返14\n—— —— —— —— ——\n🍗兴宁桥烤鸡(邱隘店)\n🈵20返15\n—— —— —— —— ——\n🍱食光解馋坊\n🈵20返15\n😍超低限时补贴，手慢无！👉6url.cn/fEGtuw\n🔥吃霸王餐0元许愿，赢取iPad、拍立得👉：sourl.cn/PGDZa2'
-        # '回头客飙升的店铺，必须拿捏👇\n\n🥓久久丫·鸭脖(古林店)\n🈵15返12\n—— —— —— —— ——\n🍲西北阁(炒饭·炒面·炒菜)\n🈵20返10\n—— —— —— —— ——\n🍗兴宁桥烤鸡(邱隘店)\n🈵20返15\n—— —— —— —— ——\n🍱食光解馋坊\n🈵20返15\n😍超低限时补贴，手慢无！👉6url.cn/fEGtuw\n🔥吃霸王餐0元许愿，赢取iPad、拍立得👉：sourl.cn/PGDZa2'
-        '回头客飙升的店铺，必须拿捏👇\n\n🥓久久丫·鸭脖(古林店)\n🈵15返12\n—— —— —— —— ——\n🍲西北阁(炒饭·炒面·炒菜)\n🈵20返10\n—— —— —— —— ——\n🍗兴宁桥烤鸡(邱隘店)\n🈵20返15\n—— —— —— —— ——\n🍱食光解馋坊\n🈵20返15\n—— —— —— —— ——\n来一餐木桶饭第一档口\n🈵20返13\n—— —— —— —— ——\n尝态·健康创意简餐\n🈵25返15\n—— —— —— —— ——\nbigbear韩国炸鸡(海曙店)\n🈵20返13\n—— —— —— —— ——\n毅动轻食·沙拉(镇海新城店)\n🈵25返18\n—— —— —— —— ——\n仙家·秘汁烤鸡（清水桥路店）\n🈵20返12\n😍超低限时补贴，手慢无！👉6url.cn/fEGtuw\n🔥吃霸王餐0元许愿，赢取iPad、拍立得👉：sourl.cn/PGDZa2'
-    ]
-    # 发送文件
-    files = [
-        # r'F:\!Code\Code_Python\WeChat-Robot\pic\快乐干饭.png',  # 图片
-        r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\dinner.jpg',  # 图片
-        # r'C:\Users\user\Desktop\2.txt',  # 文件
-        # r'C:\Users\user\Desktop\3.mp4'  # 视频
-    ]
-    for l in listen_list:
-        for m in message_list:
-            wx.SendMsg(msg=m, who=l)
-            # wx.AtAll(msg=m, who=l)
-            wx.SendFiles(filepath=files, who=l)
+    def PushLunch():
+        wx = WeChat()
+        msg_list = [
+            '🌈 【午餐不设防】 🌈\n🍔 歪麦霸王餐，让你的午餐时光，美味又自由！\n\n西北阁(炒饭·炒面·炒菜)\n🈵20返14\n兴宁桥烤鸡(邱隘店)\n🈵20返15\n食光解馋坊\n🈵20返15\n尝态·健康创意简餐\n🈵25返15\n来一餐木桶饭第一档口\n🈵20返15\n------------------\n⬇快来点链接选店啵~👇\n6url.cn/C8XKHh\n\n——————՞•・•՞——————\n🔥每日持续参与，赢取iPad、拍立得等开学礼包\n👉sourl.cn/PGDZa2'
+        ]
+        filepath = [
+            r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\dinner.jpg'
+        ]
+        PushMsg(msg_list, filepath)
 
 
-def PushSnack():
-    # 获取微信窗口对象
-    wx = WeChat()
-    # 发送消息列表
-    message_list = [
-        # '夜宵时间到，啤酒🍺烧烤🍗不能少。\n\n🍤盱眙兄弟龙虾·活龙虾(宁波汽车东站店)\n🈵50返27\n\n🍺8+1小酒馆\n🈵40返37\n\n🍻思念酒馆·现调鸡尾酒\n🈵55返18\n\n🍢永钰烤串·必出精品满\n🈵30返16\n\n歪麦霸王餐链接👇\n6url.cn/C8XKHh\n美团红包链接👇\n6url.cn/yzc5b8\n————՞•・•՞————\n[庆祝]上线参与有机会把iPad、拍立得等开学礼包带回家！\n活动：sourl.cn/PGDZa2'
-        '女孩子千万不要夜跑🏃‍♀️太危险了，\n😱万一遇到烧烤摊就完了\n\n🍤盱眙兄弟龙虾·活龙虾(宁波汽车东站店)\n🈵50返27\n—— —— —— —— ——\n🍺8+1小酒馆\n🈵40返37\n—— —— —— —— ——\n🍻思念酒馆·现调鸡尾酒\n🈵55返18\n—— —— —— —— ——\n🍢永钰烤串·必出精品满\n🈵30返16\n『歪麦』成本价点外卖\n👉6url.cn/fEGtuw\n————՞•・•՞————\n宵夜订单记得要提交哦~~\n别顾着吃忘记啦'
-    ]
-    # 发送文件
-    files = [
-        # r'F:\!Code\Code_Python\WeChat-Robot\pic\Snack.png',  # 图片
-        r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\Snack.png',  # 图片
-        # r'C:\Users\user\Desktop\2.txt',  # 文件
-        # r'C:\Users\user\Desktop\3.mp4'  # 视频
-    ]
-    for l in listen_list:
-        for m in message_list:
-            wx.SendMsg(msg=m, who=l)
-            # wx.AtAll(msg=m, who=l)
-            wx.SendFiles(filepath=files, who=l)
+    def PushTea():
+        wx = WeChat()
+        msg_list = [
+            '犯困可真是个喝奶茶的好理由呀[嘿哈]\n\n蜜雪冰城(宁波通用)\n🈵20返10\n榴小可榴莲饮品\n🈵20返12\n喜茶(宁波海曙龙湖天街店)\n🈵20返10\nF·CAFE\n🈵25返12\n273C(带梦陶然店)\n🈵30返15\n------------------\n⬇快来点链接选店啵~👇\n6url.cn/C8XKHh\n\n——————՞•・•՞——————\n🔥每日持续参与，赢取iPad、拍立得等开学礼包\n👉sourl.cn/PGDZa2'
+        ]
+        filepath = [
+            r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\tea.jpg'
+        ]
+        PushMsg(msg_list, filepath)
 
 
-def PushActivity():
-    # 获取微信窗口对象
-    wx = WeChat()
-    # 发送消息列表
-    message_list = [
-        # '这是个早餐测试消息'
-        # '【🔔歪麦资讯】\n①、必胜客自助餐全网最低价139.9元就在歪麦霸王餐'
-        # '🚀【活动置顶】开学季狂欢，会员福利享不停！🎉\n\n🎁【开学季点霸王餐返会员】开学季福利大放送！ \n30天内完成10单即享30天会员，霸王餐等你拿！\n\n🎁【开学季限时团长赛】开学季就是要拼团！ 邀请好友，赢取70麦粒，快来挑战吧！\n\n⏰【活动时间】：9月2日-9月16日，限时狂欢，抓紧机会！\n\n👥【呼朋唤友】\n快告诉你的小伙伴们，一起组队参与，让这份开学惊喜传递给更多人！🌈\n\n活动详情戳：6url.cn/C8XKHh'
-        # '[庆祝]活动一览表，[礼物]福利快速浏览\n\n1️⃣【吃霸王餐抽取iPad】🔥\n越吃越幸运，奖品有iPad、拍立得、年会员、万元红包等开学礼包，连续14天每日送一台；\n参与的宝子都能领取1天会员，人人有份\n👉开学季活动这里看：sourl.cn/PGDZa2\n\n2️⃣【活动挑战赛】🎉\na.参与迎中秋限定干饭挑战，赢24元现金[红包]\nb.邀请好友吃霸王餐，领7天会员和13元现金[红包]\nc.参与开学季限时团长赛，得70元现金[红包]\n\n3️⃣【会员领取】🔑\na.参与开学季点霸王餐活动可获得30天会员奖励\nb.进群领7天会员：6url.cn/DquG8s'
-        '周三霸王日福利纵览👇\n\n1⃣🎉【活动挑战赛】\n• 参与迎中秋限定干饭挑战，赢24元现金[红包]\n\n2⃣【独家口令】\n• 打开app-进入兑换专区-输入口令：上歪麦抽ipad 即可领取7天会员\n\n3⃣🔥【吃霸王餐抽iPad】\n• 上线参与0元许愿，都能获得1天会员，人人有份，不容错过！\n• 参与吃霸王餐活动即有机会抽取iPad、拍立得、年会员、万元红包等开学礼包！\n\n4⃣【好友助力】\n•  每邀请一位好友吃霸王餐，即可额外获得5张许愿卷；\n• 邀请上海用户吃霸王餐，双方各领23元；\n\n🔗 活动详情入口：sourl.cn/PGDZa2 即可查看更多活动详情和参与方式'
-        # '开学季活动已过半，上线0️⃣元许愿，吃霸王餐赢最后几台ipad、拍立得啦！\n\n🔥每日持续参与，赢取丰厚开学礼包！iPad、拍立得、年会员还有万元红包，连续放送至本月15号\n\n💁参与0元许愿，必得1天会员，人人有份；邀请好友助力额外获得❺张许愿卷\n\n⏰今日14:30准时揭晓昨日霸王餐幸运儿名单，看看哪些麦粉能喜提大奖！\n\n👇赶紧转发给朋友们，共享这份开学的喜悦与幸运吧！开学季活动这里看：https://sourl.cn/PGDZa2\n🔔tip: 歪麦30天会员：凭教师资格证可领（仅限教师节当日）'
-    ]
-    # 发送文件
-    files = [
-        # r'F:\!Code\Code_Python\WeChat-Robot\pic\activity.jpg',  # 图片
-        r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\activity.jpg'
-        # r'C:\Users\user\Desktop\2.txt',  # 文件
-        # r'C:\Users\user\Desktop\3.mp4'  # 视频
-    ]
-    for l in listen_list:
-        for m in message_list:
-            wx.SendMsg(msg=m, who=l)
-            # wx.AtAll(msg=m, who=l)
-            wx.SendFiles(filepath=files, who=l)
+    def PushSupper():
+        wx = WeChat()
+        msg_list = [
+            '🍲 【晚餐，不花钱的秘密】 🍴\n🥗 今夜，让歪麦的霸王餐成为你餐桌上的主角，享受美食，享受生活，最重要的是——免费！\n\n久久丫·鸭脖(古林店)\n🈵15返12\n食光解馋坊\n🈵20返15\n尝态·健康创意简餐\n🈵25返15\nbigbear韩国炸鸡(海曙店)\n🈵20返13\n毅动轻食·沙拉(镇海新城店)\n🈵25返18\n------------------\n⬇快来点链接选店啵~👇\n6url.cn/C8XKHh\n\n——————՞•・•՞——————\n🔥每日持续参与，赢取iPad、拍立得等开学礼包\n👉sourl.cn/PGDZa2'
+        ]
+        filepath = [
+            r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\dinner.jpg'
+        ]
+        PushMsg(msg_list, filepath)
 
 
-# PushBreakfast()
-# PushLunch()
-# PushTea()
-# PushSupper()
-# PushActivity()
-# PushSnack()
+    def PushSnack():
+        wx = WeChat()
+        msg_list = [
+            '🍖 【深夜食堂，美食不打烊】 🍲\n夜宵时间到，啤酒🍺烧烤🍗不能少！\n\n盱眙兄弟龙虾·活龙虾(宁波汽车东站店)\n🈵50返27\n8+1小酒馆\n🈵40返37\n思念酒馆·现调鸡尾酒\n🈵55返18\n永钰烤串·必出精品满\n🈵30返16\n------------------\n⬇快来点链接选店啵~👇\n6url.cn/C8XKHh\n\n——————՞•・•՞——————\n🔔宵夜订单记得要提交哦~~\n🔔别顾着吃忘记啦~'
+        ]
+        filepath = [
+            r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\snack.png'
+        ]
+        PushMsg(msg_list, filepath)
 
-# 定时执行任务
-schedule.every().day.at("08:30:00").do(PushBreakfast)
-schedule.every().day.at("10:00:00").do(PushActivity)
-schedule.every().day.at("10:30:00").do(PushLunch)
-schedule.every().day.at("14:00:00").do(PushTea)
-schedule.every().day.at("16:30:00").do(PushActivity)
-schedule.every().day.at("17:00:00").do(PushSupper)
-schedule.every().day.at("21:00:00").do(PushSnack)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    def PushActivity():
+        wx = WeChat()
+        msg_list = [
+            '🎉活动一览表，🎁福利快速浏览\n\n1️⃣🎉【活动挑战赛】\n• 参与迎中秋限定干饭挑战，赢24元现金🧧\n• 打开app-赚钱-点击活动二，即可参与挑战赛\n\n2️⃣🔥【吃霸王餐抽iPad】\n• 上线参与0元许愿，都能获得1天会员，人人有份，不容错过！\n• 参与吃霸王餐活动即有机会抽取iPad、拍立得、年会员、万元红包等开学礼包！\n🔗 活动详情入口：sourl.cn/PGDZa2 即可查看更多活动详情和参与方式'
+        ]
+        filepath = [
+            r'C:\Users\MM\PycharmProjects\WeChat-Robot\pic\activity.jpg'
+        ]
+        PushMsg(msg_list, filepath)
+
+
+    # PushBreakfast()
+    # PushLunch()
+    # PushTea()
+    # PushSupper()
+    # PushSnack()
+    # PushActivity()
+
+    # 定时执行任务
+    schedule.every().day.at("08:30:00").do(PushBreakfast)
+    schedule.every().day.at("10:00:00").do(PushActivity)
+    schedule.every().day.at("10:30:00").do(PushLunch)
+    schedule.every().day.at("14:00:00").do(PushTea)
+    schedule.every().day.at("16:30:00").do(PushActivity)
+    schedule.every().day.at("17:00:00").do(PushSupper)
+    schedule.every().day.at("21:00:00").do(PushSnack)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
