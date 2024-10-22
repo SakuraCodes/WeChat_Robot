4683fd395b43827756018bfbb3aa49dccfda69e1
Sakura <1467649772@qq.com>
2024-10-22 10:43:47 +0800

    push

--- select_category.py
+++ select_category.py
@@ -19,7 +19,7 @@ def rand_shop(categories):
     # 遍历选择的商家，并将商家名称和满返信息拼接到输出字符串中
     shop_info = ""
     for index, row in selected_df.iterrows():
-        name = row["店铺名称"]
+        name = row["店铺名称"].strip()
         cashback = row["返利信息"]
         shop_info += f"\n{name}\n{cashback}"
     return shop_info
